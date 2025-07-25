4113a9d1d5c88e19d671cfa1e7836c4bd9731df4
Isha-ahsI <isha142005@gmail.com>
2025-07-25 16:14:23 +0530

    add images url and name data

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,4 @@
+// userImages
 import avatar1 from "../assets/user_images/user1.jpg"
 import avatar2 from "../assets/user_images/user2.jpg"
 import avatar3 from "../assets/user_images/user3.jpg"
@@ -8,9 +9,28 @@ import avatar7 from "../assets/user_images/user7.jpg"
 import avatar8 from "../assets/user_images/user8.jpg"
 import avatar9 from "../assets/user_images/user9.jpg"
 import avatar10 from "../assets/user_images/user10.jpg"
+import avatar11 from "../assets/user_images/user11.jpg"
+import avatar12 from "../assets/user_images/user12.jpg"
+import avatar13 from "../assets/user_images/user13.jpg"
+import avatar14 from "../assets/user_images/user14.jpg"
+import avatar15 from "../assets/user_images/user15.jpg"
+
+// trustedBrandsLogo
+import logo1 from "../assets/b_logo/logo_1.png"
+import logo2 from "../assets/b_logo/logo_2.png"
+import logo3 from "../assets/b_logo/logo_3.png"
+import logo4 from "../assets/b_logo/logo_4.png"
+import logo5 from "../assets/b_logo/logo_5.png"
+import logo6 from "../assets/b_logo/logo_6.png"
+import logo7 from "../assets/b_logo/logo_7.png"
+import logo8 from "../assets/b_logo/logo_8.png"
+import logo9 from "../assets/b_logo/logo_9.png"
+import logo10 from "../assets/b_logo/logo_10.png"
+import logo11 from "../assets/b_logo/logo_11.png"
+import logo12 from "../assets/b_logo/logo_12.png"
 
 
-
+// export userImages
 export const userImages = [
   // Inner Orbit
   { orbit: "inner", className: "orbit-i1", src: avatar1 },
@@ -20,7 +40,7 @@ export const userImages = [
   // Middle Orbit
   { orbit: "middle", className: "orbit-m1", src: avatar3 },
   { orbit: "middle", className: "orbit-m2", type: "dot" },
-  { orbit: "middle", className: "orbit-m3", src: avatar4},
+  { orbit: "middle", className: "orbit-m3", src: avatar4 },
   { orbit: "middle", className: "orbit-m4", type: "dot" },
   { orbit: "middle", className: "orbit-m5", src: avatar5 },
 
@@ -34,4 +54,20 @@ export const userImages = [
   { orbit: "outer", className: "orbit-o7", type: "dot" },
   { orbit: "outer", className: "orbit-o8", type: "dot" },
   { orbit: "outer", className: "orbit-o9", type: "dot" }
+];
+
+//export trustedBrandsLogo
+export const logoImages = [
+  { src: logo1 , Name:"google" },
+  { src: logo2 , Name:"netflix" },
+  { src: logo3 , Name:"paypal" },
+  { src: logo4 , Name:"samsung" },
+  { src: logo5 , Name:"visa" },
+  { src: logo6 , Name:"amozonpay" },
+  { src: logo7 , Name:"lenovo" },
+  { src: logo8 , Name:"yahoo" },
+  { src: logo9 , Name:"canon" },
+  { src: logo10 , Name:"discover" },
+  { src: logo11 , Name:"flaticon" },
+  { src: logo12 , Name:"shopify" }
 ];
