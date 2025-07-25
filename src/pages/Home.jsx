cbfcd0aa37be71a54ff473c8856e8626113c7aa0
Isha-ahsI <isha142005@gmail.com>
2025-07-25 16:12:07 +0530

    import TrustedBrands in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/home/HeroSection'
+import TrustedBrands from '../components/home/TrustedBrands'
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <TrustedBrands />
     </div>
   )
 }
