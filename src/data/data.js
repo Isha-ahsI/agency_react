07e11db2fa80b130d25bf853c08dff5988eb65a6
Isha-ahsI <isha142005@gmail.com>
2025-07-25 12:29:55 +0530

    add data for hero-section images/avtar

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import avatar1 from "../assets/user_images/user1.jpg"
+import avatar2 from "../assets/user_images/user2.jpg"
+import avatar3 from "../assets/user_images/user3.jpg"
+import avatar4 from "../assets/user_images/user4.jpg"
+import avatar5 from "../assets/user_images/user5.jpg"
+import avatar6 from "../assets/user_images/user6.jpg"
+import avatar7 from "../assets/user_images/user7.jpg"
+import avatar8 from "../assets/user_images/user8.jpg"
+import avatar9 from "../assets/user_images/user9.jpg"
+import avatar10 from "../assets/user_images/user10.jpg"
+
+
+
+export const userImages = [
+  // Inner Orbit
+  { orbit: "inner", className: "orbit-i1", src: avatar1 },
+  { orbit: "inner", className: "orbit-i2", src: avatar2 },
+  { orbit: "inner", className: "orbit-i3", type: "dot" },
+
+  // Middle Orbit
+  { orbit: "middle", className: "orbit-m1", src: avatar3 },
+  { orbit: "middle", className: "orbit-m2", type: "dot" },
+  { orbit: "middle", className: "orbit-m3", src: avatar4},
+  { orbit: "middle", className: "orbit-m4", type: "dot" },
+  { orbit: "middle", className: "orbit-m5", src: avatar5 },
+
+  // Outer Orbit
+  { orbit: "outer", className: "orbit-o1", src: avatar6 },
+  { orbit: "outer", className: "orbit-o2", src: avatar7 },
+  { orbit: "outer", className: "orbit-o3", src: avatar8 },
+  { orbit: "outer", className: "orbit-o4", src: avatar9 },
+  { orbit: "outer", className: "orbit-o5", src: avatar10 },
+  { orbit: "outer", className: "orbit-o6", type: "dot" },
+  { orbit: "outer", className: "orbit-o7", type: "dot" },
+  { orbit: "outer", className: "orbit-o8", type: "dot" },
+  { orbit: "outer", className: "orbit-o9", type: "dot" }
+];
