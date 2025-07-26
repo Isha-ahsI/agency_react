edf9ea8d8338b8ec41922a4dfa31f829279d5042
Isha-ahsI <isha142005@gmail.com>
2025-07-26 14:10:00 +0530

    add services.jsx file in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSection from '../components/home/HeroSection'
 import TrustedBrands from '../components/home/TrustedBrands'
 import AboutUs from '../components/home/AboutUs'
+import Services from '../components/home/Services'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <HeroSection />
       <TrustedBrands />
       <AboutUs />
+      <Services />
     </div>
   )
 }
