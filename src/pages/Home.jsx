53604228ea2f145a80ddfd92a8ea19df7f9173ff
Isha-ahsI <isha142005@gmail.com>
2025-07-26 10:00:16 +0530

    add AboutUs.jsx file in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeroSection from '../components/home/HeroSection'
 import TrustedBrands from '../components/home/TrustedBrands'
+import AboutUs from '../components/home/AboutUs'
 
 const Home = () => {
   return (
